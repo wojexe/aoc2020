bc028793c5988d7671f72f878cd8d31bbc18159d
Wojciech Sierotowicz <wojtek.sierotowicz@zendesk.com>
2023-07-14 15:28:51 +0200

    Add solution template

---
+++ src/template.rb
@@ -0,0 +1,21 @@
+# typed: true
+# frozen_string_literal: true
+
+class DayXX
+  def initialize()
+    @input = File.readlines("inputs/dayXX").map(&:to_i)
+  end
+
+  def part_one
+    return nil
+  end
+
+  def part_two
+    return nil
+  end
+end
+
+solution = DayXX.new()
+
+puts solution.part_one()
+puts solution.part_two()
